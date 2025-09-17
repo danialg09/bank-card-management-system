1c73cb1a5a48735fe0222db45a7564fee01f85aa
Acer <5457892350@my.lerna.kz>
2025-09-17 14:28:26 +0300

    refactor: remove ResponseEntity and make exception HTTP codes clearer

--- src/main/java/com/example/bankcards/exception/WebAppExceptionHandler.java
+++ src/main/java/com/example/bankcards/exception/WebAppExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.example.bankcards.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
@@ -10,40 +10,39 @@ import org.springframework.web.context.request.WebRequest;
 public class WebAppExceptionHandler {
 
     @ExceptionHandler(value = RefreshTokenException.class)
-    public ResponseEntity<ErrorResponseBody> refreshTokenException(RefreshTokenException e, WebRequest request) {
-        return buildResponse(HttpStatus.FORBIDDEN, e, request);
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponseBody refreshTokenException(RefreshTokenException e, WebRequest request) {
+        return buildResponse(e, request);
     }
 
     @ExceptionHandler(value = AlreadyExistsException.class)
-    public ResponseEntity<ErrorResponseBody> alreadyExistsException(AlreadyExistsException e, WebRequest request) {
-        return buildResponse(HttpStatus.BAD_REQUEST, e, request);
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponseBody alreadyExistsException(AlreadyExistsException e, WebRequest request) {
+        return buildResponse(e, request);
     }
 
     @ExceptionHandler(value = EntityNotFoundException.class)
-    public ResponseEntity<ErrorResponseBody> entityNotFoundException(EntityNotFoundException e, WebRequest request) {
-        return buildResponse(HttpStatus.NOT_FOUND, e, request);
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponseBody entityNotFoundException(EntityNotFoundException e, WebRequest request) {
+        return buildResponse(e, request);
     }
 
-    @ExceptionHandler(value = DifferentOwnerException.class)
-    public ResponseEntity<ErrorResponseBody> differentOwnerException(DifferentOwnerException e, WebRequest request) {
-        return buildResponse(HttpStatus.BAD_REQUEST, e, request);
-    }
-
-    @ExceptionHandler(value = InsufficientFundsException.class)
-    public ResponseEntity<ErrorResponseBody> insufficientFundsException(InsufficientFundsException e, WebRequest request) {
-        return buildResponse(HttpStatus.BAD_REQUEST, e, request);
+    @ExceptionHandler({InsufficientFundsException.class, DifferentOwnerException.class})
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponseBody handleConflict(RuntimeException e, WebRequest request) {
+        return buildResponse(e, request);
     }
 
     @ExceptionHandler(value = IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponseBody> illegalArguments(IllegalArgumentException e, WebRequest request) {
-        return buildResponse(HttpStatus.BAD_REQUEST, e, request);
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponseBody illegalArguments(IllegalArgumentException e, WebRequest request) {
+        return buildResponse(e, request);
     }
 
-    private ResponseEntity<ErrorResponseBody> buildResponse(HttpStatus httpStatus, Exception e, WebRequest request) {
-        return ResponseEntity.status(httpStatus)
-                .body(ErrorResponseBody.builder()
+    private ErrorResponseBody buildResponse(Exception e, WebRequest request) {
+        return ErrorResponseBody.builder()
                         .message(e.getMessage())
                         .description(request.getDescription(false))
-                        .build());
+                        .build();
     }
 }
